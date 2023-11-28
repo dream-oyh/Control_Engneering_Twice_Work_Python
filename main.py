ff8f4c8143dba7c805df01652338b29fd1f76533
dream同学0 <10135984+o19859860010@user.noreply.gitee.com>
2023-11-28 14:48:29 +0800

    修改类定义，重构前端框架GUI

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import customtkinter
 
 from window import MyTabView
+from TabView import MyTab
 
 
 class App(customtkinter.CTk):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.tab_view = MyTabView(master=self, width=1000, height=500)
+        self.tab_view = MyTab(master=self, width=1000, height=500)
         self.tab_view.grid(row=0, column=0, padx=20, pady=20)
 
 
